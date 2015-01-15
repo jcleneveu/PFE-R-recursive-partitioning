53b7351b54e7ce06809736f5ed8b567755a6c322
Jean-Charles Leneveu <leneveu.jc@gmail.com>
2015-01-15 22:17:20 +0100

    Added algorithm parameters

--- config.R
+++ config.R
@@ -7,5 +7,10 @@ CSVfile <- "data.csv"
 columns <- c("Google.Trends", "Users.Number", "Volume.Transaction", "Miners.Gain", "Youtube.Searches")
 referencial <- "Rate"
 
+# Threshold parameters
+criterion_threshold = 0.8
+min_nb_parameters = 3
+max_nb_parameters = 3
+
 #columns <- c("c1", "c2", "c3")
 #referencial <- "c0"
