79035dd68c252b30dac70532ddb6e199f3077203
maxwy <maxime.wyka@hotmail.fr>
2015-01-19 16:11:12 +0100

    Added prediction thanks to previous data

--- prediction.R
+++ prediction.R
@@ -4,3 +4,15 @@ source(file="script.R")
 # Prediction from data and tree
 prediction <- Predict(result, newdata=parameters)
 print(prediction)
+
+################################################
+##Forecasting en fonction des données précédentes
+#library(forecast)
+#sensor <- ts(exogene$Cours) 
+#fit <- auto.arima(sensor)
+##Possible de faire une prédiction sur plus de 20 points (second paramètre)
+#fcast <- forecast(fit,20)
+#fcast
+#plot(fcast)
+#grid()
+################################################
